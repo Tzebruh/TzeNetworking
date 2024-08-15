48c88051f9f75882eccdf8b97e367cc16e3db022
Tzebruh <justincoffman.dev@outlook.com>
2024-08-14 21:26:47 -0400

    Add Dispose method to TzeTcpClient + Convert TzeTcpClient to use tabs

--- TzeTcpClient.cs
+++ TzeTcpClient.cs
@@ -7,72 +7,79 @@ namespace TzeNetworking;
 /// </summary>
 public class TzeTcpClient
 {
-    /// <summary>
-    /// The actual TcpClient being used.
-    /// </summary>
-    public TcpClient Client { get; private set; }
+	/// <summary>
+	/// The actual TcpClient being used.
+	/// </summary>
+	public TcpClient Client { get; private set; }
 
-    #region Constructors
-    /// <summary>
-    /// Creates a new TzeTcpClient. Doesn't connect to anything by default.
-    /// </summary>
-    public TzeTcpClient()
-    {
-        Client = new();
-    }
+	#region Constructors
+	/// <summary>
+	/// Creates a new TzeTcpClient. Doesn't connect to anything by default.
+	/// </summary>
+	public TzeTcpClient()
+	{
+		Client = new();
+	}
 
-    /// <summary>
-    /// Creates a new TzeTcpClient and connects to the provided address.
-    /// </summary>
-    /// <param name="connectionAddress">The URI of the address to connect to. The URI must include the port.</param>
-    public TzeTcpClient(Uri connectionAddress)
-    {
-        Client = new();
-        Client.Connect(connectionAddress.Host, connectionAddress.Port);
-    }
+	/// <summary>
+	/// Creates a new TzeTcpClient and connects to the provided address.
+	/// </summary>
+	/// <param name="connectionAddress">The URI of the address to connect to. The URI must include the port.</param>
+	public TzeTcpClient(Uri connectionAddress)
+	{
+		Client = new();
+		Client.Connect(connectionAddress.Host, connectionAddress.Port);
+	}
 
-    /// <summary>
-    /// Creates a new TzeTcpClient and connects to the provided address.
-    /// </summary>
-    /// <param name="hostname">The address to connect to. (Excluding the port.)</param>
-    /// <param name="port">The port to connect to.</param>
-    public TzeTcpClient(string hostname, int port)
-    {
-        Client = new();
-        Client.Connect(hostname, port);
-    }
-    #endregion
+	/// <summary>
+	/// Creates a new TzeTcpClient and connects to the provided address.
+	/// </summary>
+	/// <param name="hostname">The address to connect to. (Excluding the port.)</param>
+	/// <param name="port">The port to connect to.</param>
+	public TzeTcpClient(string hostname, int port)
+	{
+		Client = new();
+		Client.Connect(hostname, port);
+	}
+	#endregion
 
-    #region Connection Methods
-    /// <summary>
-    /// Connects to the provided address.
-    /// </summary>
-    /// <param name="connectionAddress">The URI of the address to connect to. The URI must include the port.</param>
-    public void Connect(Uri connectionAddress)
-    {
-        Client.Connect(connectionAddress.Host, connectionAddress.Port);
-    }
+	#region Connection Methods
+	/// <summary>
+	/// Connects to the provided address.
+	/// </summary>
+	/// <param name="connectionAddress">The URI of the address to connect to. The URI must include the port.</param>
+	public void Connect(Uri connectionAddress)
+	{
+		Client.Connect(connectionAddress.Host, connectionAddress.Port);
+	}
 
-    /// <summary>
-    /// Connects to the provided address.
-    /// </summary>
-    /// <param name="hostname">The address to connect to. (Excluding the port.)</param>
-    /// <param name="port">The port to connect to.</param>
-    public void Connect(string hostname, int port)
-    {
-        Client.Connect(hostname, port);
-    }
-    #endregion
+	/// <summary>
+	/// Connects to the provided address.
+	/// </summary>
+	/// <param name="hostname">The address to connect to. (Excluding the port.)</param>
+	/// <param name="port">The port to connect to.</param>
+	public void Connect(string hostname, int port)
+	{
+		Client.Connect(hostname, port);
+	}
+	#endregion
 
-    #region Packet Methods
-    /// <summary>
-    /// Sends the provided TzePacket to the server.
-    /// </summary>
-    /// <param name="packet">The TzePacket to send</param>
-    public void Send(TzePacket packet)
-    {
-        NetworkStream stream = Client.GetStream();
-        stream.Write(packet.SerializedPacket);
-    }
-    #endregion
+	#region Dispose
+	/// <summary>
+	/// Disposes of the internal TcpClient object.
+	/// </summary>
+	public void Dispose() => Client.Dispose();
+	#endregion
+
+	#region Packet Methods
+	/// <summary>
+	/// Sends the provided TzePacket to the server.
+	/// </summary>
+	/// <param name="packet">The TzePacket to send</param>
+	public void Send(TzePacket packet)
+	{
+		NetworkStream stream = Client.GetStream();
+		stream.Write(packet.SerializedPacket);
+	}
+	#endregion
 }
